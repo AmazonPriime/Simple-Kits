156dac88b7c356a13c8108a150d2229b05d3cf6b
AmazonPriime <lukeholland15500@gmail.com>
2019-12-27 09:00:31 +0000

    added better message handling and message to config

--- Simple Kits/src/main/java/me/amazonpriime/simplekits/SimpleKits.java
+++ Simple Kits/src/main/java/me/amazonpriime/simplekits/SimpleKits.java
@@ -12,6 +12,10 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.List;
 
 public final class SimpleKits extends JavaPlugin {
+
+    private String[][] placeholders = {{"{kit}", ""}, {"{material}", ""}, {"{quantity}", ""}, {"{kit_list}", ""}};
+    private final String prefix = getConfig().getString("messages.prefix");
+
     @Override
     public void onEnable() {
         saveDefaultConfig();
@@ -24,51 +28,67 @@ public final class SimpleKits extends JavaPlugin {
             Player player = (Player) sender;
             if (command.getName().equalsIgnoreCase("kit")) {
                 // if the user does not provide any arguments - send them a message about usage
-                if (args.length == 0) {
-                    String message = getConfig().getString("messages.usage");
-                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
-                    return false;
-                }
+                if (args.length == 0) { return infoMessage("messages.usage", player); }
+
                 // if there are no kits defined in the config - send them a message saying no kits are defined
-                if (kits == null) {
-                    String message = getConfig().getString("messages.no_kits");
-                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
-                    return false;
-                }
+                if (kits == null) { return infoMessage("messages.no_kits", player); }
+
                 // if the user uses the lists argument
-                if (args[0].equalsIgnoreCase("list")) {
-                    StringBuilder availableKits = new StringBuilder();
-                    for (String kitName : kits.getKeys(false)) {
-                        if (availableKits.length() == 0) { availableKits.append(kitName); continue;}
-                        availableKits.append(", ").append(kitName);
-                    }
-                    player.sendMessage(availableKits.toString());
-                    return true;
-                }
+                if (args[0].equalsIgnoreCase("list")) { return listArgument(kits, player, placeholders); }
+
                 // if the kit the user specifies in their argument exists then give them the items
                 if (kits.contains(args[0])) {
-                    List<String> items = getConfig().getStringList("kits." + args[0] + ".items");
-                    for (String item : items) {
-                        String[] itemArray = item.split(", ");
-                        Material material = Material.getMaterial(itemArray[0]);
-                        if (material == null) {
-                            String message = getConfig().getString("messages.invalid_item");
-                            message = message.replace("{kit}", args[0]);
-                            message = message.replace("{material}", itemArray[0]);
-                            player.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
-                            return false;
-                        }
-                        ItemStack itemStack = new ItemStack(material, Integer.parseInt(itemArray[1]));
-                        player.getInventory().addItem(itemStack);
-                    }
-                    player.sendMessage("You have received your items for kit " + args[0] + ".");
+                    placeholders[0][1] = args[0];
+                    return redeemKit(args[0], player, placeholders);
                 } else {
-                    String message = getConfig().getString("messages.does_not_exist");
-                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
+                    return infoMessage("messages.does_not_exist", player);
                 }
-                return true;
             }
         }
         return false;
+    }
+
+    // helper methods
+    private boolean infoMessage(String path, Player player) {
+        String message = prefix + " " + getConfig().getString(path);
+        player.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
+        return false;
+    }
+
+    private boolean infoMessage(String path, Player player, String[][] placeholders) {
+        String message = prefix + " " + getConfig().getString(path);
+        for (String[] placeholder_pair : placeholders) {
+            message = message.replace(placeholder_pair[0], placeholder_pair[1]);
+        }
+        player.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
+        return false;
+    }
+
+    private boolean listArgument(ConfigurationSection kits, Player player, String[][] placeholders) {
+        StringBuilder availKits = new StringBuilder();
+        player.sendMessage(kits.getKeys(false).toString());
+        for (String kitName : kits.getKeys(false)) {
+            if (availKits.length() == 0) { availKits.append(kitName); continue; }
+            availKits.append(", ").append(kitName);
+        }
+        placeholders[3][1] = availKits.toString();
+        return !infoMessage("messages.kits_list", player, placeholders);
+    }
+
+    private boolean redeemKit(String kit, Player player, String[][] placeholders) {
+        List<String> kitItems = getConfig().getStringList("kits." + kit + ".items");
+        for (String itemDetails : kitItems) {
+            String[] itemArray = itemDetails.split(", ");
+            Material item = Material.getMaterial(itemArray[0]);
+            placeholders[1][1] = itemArray[0];
+            placeholders[2][1] = itemArray[1];
+            if (item == null) {
+                return infoMessage("messages.invalid_item", player, placeholders);
+            } else {
+                player.getInventory().addItem(new ItemStack(item, Integer.parseInt(itemArray[1])));
+                return !infoMessage("messages.redeemed", player, placeholders);
+            }
+        }
+        return true;
     }
 }
